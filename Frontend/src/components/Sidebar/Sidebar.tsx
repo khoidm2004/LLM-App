7eb012ac43ca8b94e698c6539ffba5b6cb6d9cf1
khoidm2004 <khoidm2004@gmail.com>
2026-01-20 11:31:16 +0200

    Create Sidebar

--- Frontend/src/components/Sidebar/Sidebar.tsx
+++ Frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,6 +1,13 @@
-
 const Sidebar = () => {
-  return <div className="bg-gray-500">Sidebar</div>;
+  return (
+    <div className="flex h-full w-64 flex-col border-r border-border">
+      <div className="p-4">
+        <button className="w-full justify-start gap-2 cursor-pointer rounded-xl py-3 bg-[#0384c5] text-white">
+          New Summary
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
