d6f2abe23f4bb1f470fb54604e7220534a3f19bf
khoidm2004 <khoidm2004@gmail.com>
2026-01-20 08:33:32 +0200

    Create NotFound Page

--- Frontend/src/pages/NotFound/NotFound.tsx
+++ Frontend/src/pages/NotFound/NotFound.tsx
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="space-y-1 text-center">
+        <h1 className="text-[200px] font-extrabold text-[#8ac2e0]">404</h1>
+        <p className="text-3xl font-semibold text-[#8ac2e0]">
+          Oops! Page Not Found
+        </p>
+
+        <div className="mx-auto w-2/5 pt-10">
+          <button
+            className="w-full rounded-3xl bg-[#8ac2e0] py-3 text-white shadow-lg transition-colors hover:bg-white hover:text-[#8ac2e0] hover:ring-1"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Back to Home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
