01544842cb97789ea17bfe9b2ded249c2f94d907
khoidm2004 <khoidm2004@gmail.com>
2026-01-20 08:11:18 +0200

    Create LoadingPage

--- Frontend/src/pages/LoadingPage/LoadingPage.tsx
+++ Frontend/src/pages/LoadingPage/LoadingPage.tsx
@@ -1,5 +1,11 @@
+import { ScaleLoader } from "react-spinners";
+
 const LoadingPage = () => {
-  return <div>LoadingPage</div>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <ScaleLoader color="#0384c5" width={15} height={100}/>
+    </div>
+  );
 };
 
 export default LoadingPage;
